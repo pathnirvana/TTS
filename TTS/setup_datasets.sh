134a856f47976af3769acc28bf83741af08e32d4
pathnirvana <pathnirvana@gmail.com>
2021-01-30 01:54:37 +0700

    switch dataset

--- TTS/setup_datasets.sh
+++ TTS/setup_datasets.sh
@@ -3,6 +3,6 @@ mkdir -p /home/models/sinhala  /home/models/phoneme_cache
 cd /home
 mkdir -p datasets/sinhala
 cd datasets/sinhala
-wget https://github.com/pathnirvana/tacotron2/releases/download/1/sinhala.zip
+wget https://github.com/pathnirvana/TTS/releases/download/1/sinhala.zip
 unzip sinhala.zip
 cd /home
