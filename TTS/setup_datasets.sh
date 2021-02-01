bf72300f17e6ca1fc8b53bc7cde1766b1016f7ed
pathnirvana <pathnirvana@gmail.com>
2021-02-01 17:15:40 +0700

    add drive paths

--- TTS/setup_datasets.sh
+++ TTS/setup_datasets.sh
@@ -1,8 +1,8 @@
-mkdir -p /home/models/sinhala  /home/models/phoneme_cache
+mkdir -p /home/drive/models/sinhala  /home/drive/models/phoneme_cache
 
-cd /home
+cd /home/drive
 mkdir -p datasets/sinhala
 cd datasets/sinhala
 wget https://github.com/pathnirvana/TTS/releases/download/1/sinhala.zip
 unzip sinhala.zip
-cd /home
+cd /home/drive
